1f68c546c234be16c7617c324e6153e39f50e797
Andrin Bertschi <mail@abertschi.ch>
2015-02-10 01:01:12 +0100

    translation misc

--- harbour-wlan-keyboard/translations/harbour-wlan-keyboard.ts
+++ harbour-wlan-keyboard/translations/harbour-wlan-keyboard.ts
@@ -22,10 +22,6 @@
 <context>
     <name>websocket_server</name>
     <message>
-        <source>QWsServer error : %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>ping: %1 ms</source>
         <translation type="unfinished"></translation>
     </message>
