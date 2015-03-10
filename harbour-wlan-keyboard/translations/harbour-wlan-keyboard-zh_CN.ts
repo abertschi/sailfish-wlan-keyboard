ea2000f8da0faa58da37fd87e1a29095b3ea40ff
TylerTemp <tylertempdev@gmail.com>
2015-03-11 01:01:06 +0800

    add Chinese Simplified translation

---
+++ harbour-wlan-keyboard/translations/harbour-wlan-keyboard-zh_CN.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Not running</source>
+        <translation>没有运行</translation>
+    </message>
+    <message>
+        <source>Running at</source>
+        <translation>运行于</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <source>Configuration</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <source>Start server</source>
+        <translation>开启服务</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>地址</translation>
+    </message>
+    <message>
+        <source>Browse</source>
+        <translation>浏览</translation>
+    </message>
+    <message>
+        <source>Server is running</source>
+        <translation>服务正在运行</translation>
+    </message>
+    <message>
+        <source>Server is not running</source>
+        <translation>服务没有运行</translation>
+    </message>
+    <message>
+        <source>Connect your phone to a WLAN network.</source>
+        <translation>将你的手机连接到无线局域网络。</translation>
+    </message>
+</context>
+<context>
+    <name>websocket_server</name>
+    <message>
+        <source>ping: %1 ms</source>
+        <translation>ping: %1 ms</translation>
+    </message>
+</context>
+</TS>
